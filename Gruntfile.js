56e67f4ecb17ab5473958cb4366b04b1c3b4f5d7
Justin Livi <justin@brinkbit.com>
2015-11-27 21:06:23 -0500

    Tool build step
    Phaser should be installed as an npm package and should be included
    in the build within compilation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,49 @@
 'use strict';
 
 module.exports = function initGrunt( grunt ) {
+    [
+        'grunt-contrib-clean',
+        'grunt-contrib-copy'
+    ]
+    .forEach( grunt.loadNpmTasks );
+
     require( 'load-grunt-tasks' )( grunt );
 
     grunt.initConfig({
 
         pkg: grunt.file.readJSON( 'package.json' ),
 
+        clean: {
+            build: 'build'
+        },
+
         eslint: {
             target: [ 'src/**/*.js', 'test/**/*.js', 'Gruntfile.js' ]
+        },
+
+        copy: {
+            build: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'src/',
+                        src: [ '**' ],
+                        dest: 'build/'
+                    },
+                    {
+                        expand: true,
+                        cwd: 'node_modules/phaser/build/',
+                        src: [ 'phaser.map', 'phaser.min.js' ],
+                        dest: 'build/js/libs/'
+                    }
+                ]
+            }
         }
 
     });
+
+    grunt.registerTask( 'build', [
+        'clean',
+        'copy'
+    ]);
 };
